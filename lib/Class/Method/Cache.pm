01b4e4fe344cb5c5c4b83554da36b66b8a21e22f
Robert Rothenberg <rrwo@cpan.org>
2018-05-05 00:57:02 +0100

    Add stub POD

--- lib/Class/Method/Cache.pm
+++ lib/Class/Method/Cache.pm
@@ -1,5 +1,7 @@
 package Class::Method::Cache;
 
+# ABSTRACT: Cache method results using Cache::FastMmap
+
 use v5.10.1;
 
 use strict;
@@ -14,6 +16,50 @@ our $VERSION = 'v0.1.0';
 
 our @EXPORT    = qw/ cache /;
 our @EXPORT_OK = @EXPORT;
+
+=head1 SYNOPSIS
+
+  package MyClass;
+
+  use Class::Method::Cache;
+
+  sub my_method {
+    ...
+  }
+
+  cache 'my_method' => (
+     serializer  => 'storable',
+     expire_time => '1h',
+  );
+
+=head1 DESCRIPTION
+
+This package allows you to easily cache the results of a method call
+using L<Cache::FastMmap>.
+
+=head1 EXPORTS
+
+=head2 C<cache>
+
+  cache $method => %options;
+
+This wraps the C<$method> with a function that caches the return value.
+
+It assumes that the method returns a defined scalar value and that the
+method arguments are serialisable.
+
+The C<%options> are used to configure L<Cache::FastMmap>.
+
+A special option called C<key_cb> is used to provide a custom
+key-generation function.  If none is specified, then
+L<Object::Signature> is used.
+
+The function should expect a single argument with an array reference
+corresponding to the original method call parameters:
+
+  $key_cb->( [ $self, @_ ] );
+
+=cut
 
 sub cache {
     my ( $method, %options ) = @_;
@@ -37,5 +83,13 @@ sub cache {
     };
 
 }
+
+=head1 SEE ALSO
+
+L<Cache::FastMmap>
+
+L<Object::Signature>
+
+=cut
 
 1;
